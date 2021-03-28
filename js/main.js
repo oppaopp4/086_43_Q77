5e4c48018f5c1f8d593d339b1c0108a14d4df73e
hapi2 <rock666gemini69@gmail.com>
2021-03-28 21:53:53 +0900

    魔王戦追加

---
+++ js/main.js
@@ -0,0 +1,57 @@
+Encounter();
+// let cb = document.getElementById('cb'); // チェックボックス
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', saiyomikomi);
+
+
+// let enemy = new Monster(enemies[0].name, enemies[0].hp);
+function Encounter() {
+  let enemies = [
+    { name: "スライム", hp: 10 },
+    { name: "こうもり", hp: 20 },
+    { name: "魔法使い", hp: 30 },
+    { name: "ゴーレム", hp: 40 },
+    { name: "ドラゴン", hp: 50 },
+    // {name: "魔王", hp: 60},
+  ];
+  let enemy_list = [];
+  let ninzuu = Math.floor(Math.random() * 6) + 1;
+  console.log(ninzuu);
+
+  // if (cb.checked) ninzuu = 6; // チェックボックスがチェックの時、強制魔王戦
+
+  if (ninzuu === 6) {
+    enemy_list = LastBattle();
+    comment.innerHTML = "魔王戦！</br></br>" + "絶対に負けられない戦いです！</br>";
+  } else {
+    for (let i = 0; i < ninzuu; i++) {
+      let n = Math.floor(Math.random() * enemies.length);
+      enemy_list.push(new Monster(enemies[n].name, enemies[n].hp));
+      comment.innerHTML += enemy_list[i].name + "が現れた！</br>";
+    }
+  }
+  return enemy_list;
+}
+
+function LastBattle() {
+  let last_enemy_list = [
+    { name: "魔法使い", hp: 30 },
+    { name: "ドラゴン", hp: 50 },
+    { name: "魔王", hp: 100 },
+    { name: "ドラゴン", hp: 50 },
+    { name: "ゴーレム", hp: 40 },
+  ];
+  
+  let enemy = [];
+
+  last_enemy_list.forEach(e => {
+    enemy.push(new Monster(e.name, e.hp));
+  });
+
+  return enemy;
+}
+
+function saiyomikomi() {
+  window.location.reload();
+}
--- js/monster.js
+++ js/monster.js
@@ -1,5 +1,5 @@
-class Monster{
-    constructor(name, hp){
+class Monster {
+    constructor(name, hp) {
         this.name = name;
         this.hp = hp;
 
@@ -9,7 +9,7 @@ class Monster{
         this.Init();
     }
 
-    Init(){
+    Init() {
         let comment = document.getElementById('comment');
 
         let monsters = document.getElementById('monsters');
@@ -25,38 +25,35 @@ class Monster{
 
         this.ref.addEventListener('animationend', () => {
             this.ref.classList.remove("damage_motion");
-            if(!this.alive){
+            this.ref.classList.remove("kaisin_motion");
+
+            if (!this.alive) {
                 this.ref.classList.add("nodisp");
                 comment.innerHTML += this.name + "をやっつけた！";
+                console.log(monsters);
+                // if () {
+                //     comment.innerHTML += "戦いに勝利した！";
+                // }
             }
         });
     }
 
-    damage(damage){
+    damage(damage) {
+        comment.innerHTML = "";
+
+        let r = Math.floor(Math.random() * 4);
+        if (r == 0) {
+            comment.innerHTML += "会心の一撃！！</br>";
+            damage *= 10;
+            this.ref.classList.add("kaisin_motion");
+        } else {
+            this.ref.classList.add("damage_motion");
+        }
+
         this.hp -= damage;
-        if(this.hp <= 0) this.alive = false;
-        comment.innerHTML = this.name + "に " + damage + " ポイントの<br>ダメージをあたえた！<br>";
+        if (this.hp <= 0) this.alive = false;
+        comment.innerHTML += this.name + "に " + damage + " ポイントの<br>ダメージをあたえた！<br>";
 
         console.log(this.name + ":" + this.hp);
     }
-}
-
-// Encounter();
-
-let enemies =[
-    {name: "スライム", hp: 10},
-    {name: "こうもり", hp: 20},
-    {name: "魔法使い", hp: 30},
-    {name: "ゴーレム", hp: 40},
-    {name: "ドラゴン", hp: 50},
-    {name: "魔王", hp: 60},
-];
-let enemy = new Monster(enemies[0].name, enemies[0].hp);
-// let enemy = "";
-// function Encounter() {
-//     let ninzuu = Math.floor(Math.random() * 5) + 1;
-//     for (let i = 0; i < ninzuu; i++) {
-//         let n = Math.floor(Math.random() * 5);
-//         enemy = new Monster(enemies[n].name, enemies[n].hp)[i];
-//     }
 }
